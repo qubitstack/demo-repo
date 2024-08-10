173a409359592c1c3a3b6202ce1d27b397bf55eb
qubitstack <qubit45@gmail.com>
2024-08-11 01:19:24 +0530

    Update main.cpp

--- 3.Conditional Statements/main.cpp
+++ 3.Conditional Statements/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream> // Program to find the max of two numbers.
+using namespace std;
 
+int main()
+{
+  int a,b;
+  cout<<"Enter the numbers:\t";
+  cin>>a>>b;
+  if(a>b)
+  {
+    cout<<"A is greater";
+  }
+  else
+  {
+    cout<<"B is greater";
+  }
+  return 0;
+}
